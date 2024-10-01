2182f3257cd2ece63c6d630670b4e7725e483781
kickbuttowski80 <pazpaz25@gmail.com>
2024-09-30 19:58:48 -0700

    bring back intersection observer for no mobile view and clean up

--- script.js
+++ script.js
@@ -1,22 +1,17 @@
-document.querySelectorAll('#hamburger-menu  a').forEach((item) => {
+document.querySelectorAll('nav a, #hamburger-menu  a').forEach((item) => {
   console.log('item', item.hash);
   const targetElement = document.getElementById(item.hash.split('#')[1]);
   if (!targetElement) return;
   const observer = new IntersectionObserver(
     (entries) => {
       entries.forEach((entry) => {
-        //    debugger;
-        console.log(
-          'entry target',
-          entry.target,
-          entry.isIntersecting,
-          targetElement
-        );
-        if (entry.isIntersecting) item.classList.add('active');
-        else item.classList.remove('active');
+        if (entry.isIntersecting) 
+            item.classList.add('active');
+        else 
+        item.classList.remove('active');
       });
     },
-    { threshold: [0.4, 0.5] }
+    { threshold:[0.1, 0.5] }
   );
   observer.observe(targetElement);
 });
