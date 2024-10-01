30a0c289d0d6330e490a4ae051f7632ff5a97573
kickbuttowski80 <pazpaz25@gmail.com>
2024-09-30 19:25:06 -0700

    add active section to menu items

---
+++ script.js
@@ -0,0 +1,19 @@
+document.querySelectorAll('#hamburger-menu  a').forEach((item) => {
+  console.log('item', item.hash);
+  const targetElement = document.getElementById(item.hash.split('#')[1]);
+  if (!targetElement) return;
+  const observer = new IntersectionObserver(
+    (entries) => {
+      entries.forEach((entry) => {
+        //    debugger;
+           console.log('entry target', entry.target, entry.isIntersecting, targetElement)
+        if (entry.isIntersecting) 
+            item.classList.add('active');
+        else 
+        item.classList.remove('active');
+      });
+    },
+    { threshold: 0.5 }
+  );
+  observer.observe(targetElement);
+});
