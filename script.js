e7c962b43feb00c47854c7ff771daad16b009a55
kickbuttowski80 <pazpaz25@gmail.com>
2024-10-02 18:54:55 -0700

    use onload event Fixes #5

--- script.js
+++ script.js
@@ -1,27 +1,25 @@
-document.addEventListener('DOMContentLoaded', function () {
-  setTimeout(() => {
-document.querySelectorAll('nav a, #hamburger-menu  a').forEach((item) => {
- 
-  
-  const targetElement = document.getElementById(item.hash.split('#')[1]);
-  if (!targetElement) return;
-  let thresholdValue;
-  if (window.innerWidth < 768) {
-    thresholdValue = 0.1111; 
-  } else {
-    thresholdValue = 0.5; 
-  }
+document.addEventListener('onLoad', function () {
+  // setTimeout(() => {
+    document.querySelectorAll('nav a, #hamburger-menu  a').forEach((item) => {
+      const targetElement = document.getElementById(item.hash.split('#')[1]);
+      if (!targetElement) return;
+      let thresholdValue;
+      if (window.innerWidth < 768) {
+        thresholdValue = 0.1111;
+      } else {
+        thresholdValue = 0.5;
+      }
 
-  const observer = new IntersectionObserver(
-    (entries) => {
-      entries.forEach((entry) => {
-        if (entry.isIntersecting) item.classList.add('active');
-        else item.classList.remove('active');
-      });
-    },
-    { threshold: thresholdValue }
-  );
-  observer.observe(targetElement);
-});
-}, 50);
+      const observer = new IntersectionObserver(
+        (entries) => {
+          entries.forEach((entry) => {
+            if (entry.isIntersecting) item.classList.add('active');
+            else item.classList.remove('active');
+          });
+        },
+        { threshold: thresholdValue }
+      );
+      observer.observe(targetElement);
+    });
+  // }, 0);
 });
