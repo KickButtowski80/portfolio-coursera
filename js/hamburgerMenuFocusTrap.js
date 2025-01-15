277bb4f0dd4748baec91bf49e13eeb69448ba2f6
KickBUttowski80 <pazpaz25@gmail.com>
2025-01-14 16:58:43 -0800

    rid of the console log

--- js/hamburgerMenuFocusTrap.js
+++ js/hamburgerMenuFocusTrap.js
@@ -3,8 +3,6 @@ document.addEventListener("DOMContentLoaded", (event) => {
 
   hamburgerIcon.addEventListener("change", (event) => {
     const isMenuExpanded = event.target.getAttribute("aria-expanded");
-    console.log("Menu is expanded:", isMenuExpanded);
-
     if (isMenuExpanded === "true") {
       document.querySelector("#back-to-top").setAttribute("tabindex", "-1");
     } else {
