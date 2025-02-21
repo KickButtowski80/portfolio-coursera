4741fdb8edf61577f7017de33a2176f42dde5df4
KickBUttowski80 <pazpaz25@gmail.com>
2025-02-20 20:10:33 -0800

    check for no empty spaces and input must contains spaces and punctions

--- js/recommendations-form/formValidator.js
+++ js/recommendations-form/formValidator.js
@@ -1,15 +1,18 @@
 // formValidator.js
 
-document.addEventListener('DOMContentLoaded', function() {
-    const textarea = document.getElementById('recommendation-author-message');
-  
-    textarea.addEventListener('input', function() {
-      if (this.value.trim() === '') {
-        this.setCustomValidity('Please enter a recommendation (no empty spaces).');
-      } else {
-        this.setCustomValidity('');
-      }
-      this.reportValidity();
-    });
+document.addEventListener("DOMContentLoaded", function () {
+  const textarea = document.getElementById("recommendation-author-message");
+
+  textarea.addEventListener("input", function () {
+    let message = "";
+    let textAreaValue = this.value.trim();
+    const regex = /[\s.,!?]/;
+    if (textAreaValue === "") {
+      message = "Please enter a recommendation (no empty spaces).";
+    } else if (!regex.test(textAreaValue)) {
+      message = "Input must contain spaces or punctuation.";
+    }
+    this.setCustomValidity(message);
+    this.reportValidity();
   });
-  
+});
