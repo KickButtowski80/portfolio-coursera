d5007d756dd01945436380094628210918c2302b
KickBUttowski80 <pazpaz25@gmail.com>
2025-02-28 21:00:34 -0800

    add anonymous to reading name cuz name is optional

--- js/recommendations-form/readFormData.js
+++ js/recommendations-form/readFormData.js
@@ -1,7 +1,7 @@
 // Export a function to read form data
 export function readFormData(form) {
     const formData = {
-        name: form['recommendation-author-name'].value.trim(),
+        name: form['recommendation-author-name'].value.trim() || "Anonymous",
         recommendation: form['recommendation-author-message'].value.trim()
     };
     return formData;
