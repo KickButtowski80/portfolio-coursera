5677ea04eacd4e1a918f5f100ad5e5f9efd519bb
KickBUttowski80 <pazpaz25@gmail.com>
2025-03-04 20:39:57 -0800

    set Loading State for submit button

--- js/recommendations-form/formHandler.js
+++ js/recommendations-form/formHandler.js
@@ -7,19 +7,22 @@ import { showNotification } from "./notification.js";
 document.addEventListener("DOMContentLoaded", function () {
     const form = document.querySelector(".recommendation-form");
     const outputDiv = document.querySelector(".recommendation-cards");
-
+    const submitButton = form.querySelector("button[type='submit']");
     // Display saved recommendations on page load
     displaySavedRecommendations(outputDiv);
 
     form.addEventListener("submit", async function (event) {
         event.preventDefault();
+        submitButton.disabled = true;
+        submitButton.textContent = "Submitting...";
+
+        
         const formData = readFormData(form);
         formData['displayDate'] = new Date().toLocaleDateString('en-US', {
           year: 'numeric',
           month: 'long',
           day: 'numeric'
         });
-       
         try {
             await addDoc(collection(db, "recommendations"), {
                 name: formData.name,
@@ -29,9 +32,13 @@ document.addEventListener("DOMContentLoaded", function () {
             showNotification("Recommendation submitted successfully!", "success");
             displayFormData(formData, outputDiv);
             form.reset();
+            submitButton.disabled = false;
+            submitButton.textContent = "Submit";
 
         } catch (error) {
             showNotification(`An error occurred: ${error.message}. Please try again.`, "error");
+            submitButton.disabled = false;
+            submitButton.textContent = "Submit";
         }
     });
 });
