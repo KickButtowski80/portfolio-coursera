23cbb49dbd3a88937f248dee8863d6b0b27d6649
KickBUttowski80 <pazpaz25@gmail.com>
2025-03-08 17:46:45 -0800

    set one entry point for all the js files

---
+++ main.js
@@ -0,0 +1,12 @@
+import './js/brickRandomOpacity.js';
+import './js/floatBlobInBoxes.js';
+import './js/hamburgerMenuFocusTrap.js';
+import './js/printBodyElementsTabOrder.js';
+import './js/recommendations-form/notification.js';
+import './js/recommendations-form/characterCount.js';
+import './js/recommendations-form/displayFormData.js';
+import './js/index.js';
+import './js/recommendations-form/readFormData.js';
+import './js/script.js';
+import './js/todayCalendar.js';
+import './js/config/firebase.js';
