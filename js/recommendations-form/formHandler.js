ee11aaaeb278f770f314e3766c721c3764c990af
KickBUttowski80 <pazpaz25@gmail.com>
2025-03-08 19:16:12 -0800

    console log for dev mode

--- js/recommendations-form/formHandler.js
+++ js/recommendations-form/formHandler.js
@@ -1,6 +1,6 @@
 import { readFormData } from "./readFormData.js";
 import { displayFormData } from "./displayFormData.js";
-import { addFirestoreDocument } from '../config/firebase.js';
+import { addFirestoreDocument } from "../config/firebase.js";
 import { displaySavedRecommendations } from "./fetchRecommendations.js"; // New import
 import { showNotification } from "./notification.js";
 import { setupCharacterCount } from "./characterCount.js";
@@ -12,7 +12,9 @@ export function handleFormSubmit(event) {
   // Honeypot validation
   const honeypotField = document.getElementById("website").value;
   if (honeypotField) {
-    console.log("Spam detected: Honeypot field was filled out.");
+    if (import.meta.env.DEV) {
+      console.log("Spam detected: Honeypot field was filled out.");
+    }
     showNotification("Spam detected!", "error");
     return; // Stop further execution
   }
@@ -27,25 +29,25 @@ export function handleFormSubmit(event) {
     month: "long",
     day: "numeric",
   });
-  
-  addFirestoreDocument('recommendations', formData)
-  .then(() => {
-    showNotification("Recommendation submitted successfully!", "success");
-    const outputDiv = document.querySelector(".recommendation-cards");
-    displayFormData(formData, outputDiv);
-    event.target.reset();
-    submitButton.focus();
-    submitButton.disabled = false;
-    submitButton.textContent = "Submit";
-  })
-  .catch((error) => {
-    showNotification(
-      `An error occurred: ${error.message}. Please try again.`,
-      "error"
-    );
-    submitButton.disabled = false;
-    submitButton.textContent = "Submit";
-  });
+
+  addFirestoreDocument("recommendations", formData)
+    .then(() => {
+      showNotification("Recommendation submitted successfully!", "success");
+      const outputDiv = document.querySelector(".recommendation-cards");
+      displayFormData(formData, outputDiv);
+      event.target.reset();
+      submitButton.focus();
+      submitButton.disabled = false;
+      submitButton.textContent = "Submit";
+    })
+    .catch((error) => {
+      showNotification(
+        `An error occurred: ${error.message}. Please try again.`,
+        "error"
+      );
+      submitButton.disabled = false;
+      submitButton.textContent = "Submit";
+    });
 }
 
 // Initialize form functionality
@@ -53,11 +55,11 @@ export function initializeForm() {
   document.addEventListener("DOMContentLoaded", function () {
     const form = document.querySelector(".recommendation-form");
     const outputDiv = document.querySelector(".recommendation-cards");
-    
+
     // Display saved recommendations on page load
     displaySavedRecommendations(outputDiv);
     setupCharacterCount(form);
-    
+
     // Add event listener using the exported function
     form.addEventListener("submit", handleFormSubmit);
   });
