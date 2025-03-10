df931a13cd549fd813b5863c32de6fce89e151e6
KickBUttowski80 <pazpaz25@gmail.com>
2025-03-10 08:33:30 -0700

    add manulCheunks back

--- vite.config.js
+++ vite.config.js
@@ -7,15 +7,18 @@ export default defineConfig({
   build: {
     outDir: "dist",
     sourcemap: false,
-    minify: "esbuild", // Explicitly declared
+    minify: "esbuild",
     cssCodeSplit: true,
     cssMinify: true,
     assetsInlineLimit: 4096,
     rollupOptions: {
       input: {
-        main: resolve(__dirname, "index.html")
+        main: resolve(__dirname, "index.html"),
       },
       output: {
+        manualChunks: {
+          vendor: ["firebase/app", "firebase/firestore", "@fortawesome/fontawesome-free"]
+        },
         assetFileNames: (assetInfo) => {
           const imgType = /\.(png|jpe?g|gif|svg|webp|avif)$/;
           if (assetInfo.name && imgType.test(assetInfo.name)) {
@@ -23,10 +26,31 @@ export default defineConfig({
           }
           return 'assets/[name]-[hash][extname]';
         }
+      },
+      treeshake: {
+        moduleSideEffects: false,
+        propertyReadSideEffects: false,
+        tryCatch: false
+      }
+    },
+  },
+  optimizeDeps: {
+    include: ["firebase/app", "firebase/firestore", "@fortawesome/fontawesome-free"]
+  },
+  server: {
+    watch: {
+      usePolling: true,
+    },
+  },
+  css: {
+    modules: false,
+    preprocessorOptions: {
+      scss: {
+        additionalData: ''
       }
     }
   },
-  optimizeDeps: {
-    include: ["firebase/app", "firebase/firestore"]
+  define: {
+    'process.env': process.env
   }
 });
