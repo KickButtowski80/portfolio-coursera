1dd6f6459a099eb19d898eeb2d11911507bb04b8
KickBUttowski80 <pazpaz25@gmail.com>
2025-03-09 21:25:59 -0700

    update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -7,52 +7,26 @@ export default defineConfig({
   build: {
     outDir: "dist",
     sourcemap: false,
-    minify: "esbuild",
+    minify: "esbuild", // Explicitly declared
     cssCodeSplit: true,
     cssMinify: true,
     assetsInlineLimit: 4096,
     rollupOptions: {
       input: {
-        main: resolve(__dirname, "index.html"),
+        main: resolve(__dirname, "index.html")
       },
       output: {
-        // manualChunks: undefined
-        // // {
-        // //   vendor: ["firebase/app", "firebase/firestore", "@fortawesome/fontawesome-free"]
-        // // }
-        // ,
-        // assetFileNames: (assetInfo) => {
-        //   const imgType = /\.(png|jpe?g|gif|svg|webp|avif)$/;
-        //   if (assetInfo.name && imgType.test(assetInfo.name)) {
-        //     return 'assets/img/[name]-[hash][extname]';
-        //   }
-        //   return 'assets/[name]-[hash][extname]';
-        // }
-      },
-      treeshake: {
-        moduleSideEffects: false,
-        propertyReadSideEffects: false,
-        tryCatch: false
-      }
-    },
-  },
-  optimizeDeps: {
-    include: ["firebase/app", "firebase/firestore", "@fortawesome/fontawesome-free"]
-  },
-  server: {
-    watch: {
-      usePolling: true,
-    },
-  },
-  css: {
-    modules: false,
-    preprocessorOptions: {
-      scss: {
-        additionalData: ''
+        assetFileNames: (assetInfo) => {
+          const imgType = /\.(png|jpe?g|gif|svg|webp|avif)$/;
+          if (assetInfo.name && imgType.test(assetInfo.name)) {
+            return 'assets/img/[name]-[hash][extname]';
+          }
+          return 'assets/[name]-[hash][extname]';
+        }
       }
     }
   },
-  define: {
-    'process.env': process.env
+  optimizeDeps: {
+    include: ["firebase/app", "firebase/firestore"]
   }
 });
