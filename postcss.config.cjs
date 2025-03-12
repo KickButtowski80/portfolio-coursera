3be1f0a62e3d5781b9dc287b18c0d6b9df5fbb78
KickBUttowski80 <pazpaz25@gmail.com>
2025-03-11 17:29:01 -0700

    - remove css-declaration-sorter
    - add @fullhuman/postcss-purgecss and postcss again

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,12 +1,14 @@
 // postcss.config.cjs
+// postcss.config.cjs
 module.exports = {
     plugins: [
       require('@fullhuman/postcss-purgecss')({
-        content: ['./index.html', './src/**/*.{js,ts,jsx,tsx,vue,html}'],
+        content: ['./index.html', './js/**/*.js'],
         defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
         safelist: {
-          standard: ['html', 'body'],
-          deep: [/^fa-/]
+          standard: ['html', 'body', /^fa-/],
+          deep: [/^modal-/, /^carousel-/],
+          greedy: [/^nav-/]
         }
       })
     ]
