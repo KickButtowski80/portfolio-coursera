109de0f95ec8d916a21ee110c646c4329274c97f
KickBUttowski80 <pazpaz25@gmail.com>
2025-03-11 19:34:20 -0700

    comment out some key values

--- vite.config.js
+++ vite.config.js
@@ -6,13 +6,13 @@ export default defineConfig({
   build: {
     outDir: "dist",
     sourcemap: false,
-    minify: "terser",
-    terserOptions: {
-      compress: true,
-      mangle: true,
-    },
-    cssCodeSplit: true,
-    cssMinify: true,
+    // minify: "terser",
+    // terserOptions: {
+    //   compress: true,
+    //   mangle: true,
+    // },
+    // cssCodeSplit: true,
+    // cssMinify: true,
     assetsInlineLimit: 4096,
     rollupOptions: {
       input: {
@@ -33,42 +33,6 @@ export default defineConfig({
       },
     },
   },
-  plugins: [
-    sharp({
-      force: true, // Force image processing even if the output file exists
-      include: /\.(jpe?g|png|gif|webp)$/i, // File formats to process
-      exclude: /node_modules/, // Exclude node_modules
-      removeMetadata: true, // Remove image metadata
-      defaultOptions: {
-        jpeg: {
-          quality: 80,
-          progressive: true,
-        },
-        png: {
-          quality: 80,
-          compressionLevel: 9,
-        },
-        webp: {
-          quality: 80,
-          lossless: false,
-          effort: 6,
-        },
-        gif: {
-          quality: 80,
-        },
-        avif: {
-          quality: 80,
-          effort: 9,
-        },
-        resize: {
-          width: 1920, // Max width
-          height: 1080, // Max height
-          fit: 'inside', // Maintain aspect ratio
-          withoutEnlargement: true, // Don't upscale small images
-        }
-      }
-    })
-  ],
   css: {
     modules: false,
     preprocessorOptions: {
