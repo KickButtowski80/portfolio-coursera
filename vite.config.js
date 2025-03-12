f90beff97778109f09d524e7cad21e0ea57c3c82
KickBUttowski80 <pazpaz25@gmail.com>
2025-03-12 10:58:12 -0700

    use terser for minifying js files

--- vite.config.js
+++ vite.config.js
@@ -54,7 +54,32 @@ export default defineConfig({
   build: {
     outDir: "dist",
     sourcemap: false, // No sourcemaps for production
-    minify: true, // Uses esbuild (faster than Terser)
+    minify: "terser",
+    terserOptions: {
+      compress: {
+        ecma: 2020,
+        warnings: false,
+        comparisons: false,
+        inline: 2,
+        drop_console: true,
+        drop_debugger: true,
+        pure_funcs: ['console.log', 'console.info', 'console.debug'],
+        passes: 3,
+      },
+      mangle: {
+        safari10: true,
+        properties: {
+          regex: /^_/,
+        },
+      },
+      format: {
+        comments: false,
+        ecma: 2020,
+      },
+      toplevel: true,
+      keep_classnames: false,
+      keep_fnames: false,
+    },
     cssCodeSplit: true, // Splits CSS for better optimization
     assetsInlineLimit: 4096, // Inline assets under 4KB
     rollupOptions: {
